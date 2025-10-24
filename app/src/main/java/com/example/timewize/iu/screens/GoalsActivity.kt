d6a269b80f6450f8d602f6bc68f2d718d76f13fc
JuanpaAA304 <hsromerosara@gmail.com>
2025-10-23 20:16:10 -0500

    App TimeWize de Mateo

---
+++ app/src/main/java/com/example/timewize/iu/screens/GoalsActivity.kt
@@ -0,0 +1,21 @@
+package com.example.timewize.iu.screens
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.timewize.R
+
+class GoalsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_goals)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
---
+++ app/src/main/java/com/example/timewize/iu/screens/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.example.timewize.iu.screens
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.timewize.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home) // Asegúrate que coincida con tu XML
+    }
+}
